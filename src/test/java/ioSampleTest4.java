3b129d6f056adefae28202f755f708ac2145ca0a
Israt Jahan Jui <jahanjuiisrat@gmail.com>
2020-08-09 17:23:43 +0600

    Update ioSampleTest4.java

--- src/test/java/ioSampleTest4.java
+++ src/test/java/ioSampleTest4.java
@@ -64,19 +64,7 @@ public class ioSampleTest4 {
 
     @Test
     public void basicTest () throws InterruptedException, IOException {
-        //Click and pass Splash
-//        wait.until(ExpectedConditions.visibilityOfElementLocated
-//                (By.id("com.isinolsun.app:id/animation_view"))).click();
-
-        //Click and pass Tutorial
-//        wait.until(ExpectedConditions.visibilityOfElementLocated
-//                (By.id("com.isinolsun.app:id/btn_skip"))).click();
-
-        //Click to "Is Ariyorum" Button
-//        wait.until(ExpectedConditions.visibilityOfElementLocated
-//                (By.id("com.isinolsun.app:id/bluecollar_type_button"))).click();
-
-
+      
 //        //Notification Allow
         if (driver.findElements(By.id("com.android.packageinstaller:id/permission_allow_button")).size()>0) {
             driver.findElements(By.id("com.android.packageinstaller:id/permission_allow_button")).get(0).click();
